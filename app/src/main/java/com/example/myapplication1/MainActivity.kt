f8032165d445fc21d280e58541566459e8179944
trihd2 <trihd2@vng.com.vn>
2022-06-17 09:27:29 +0700

    create button, request recording permission, media recorder, timer with handler

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,11 +1,140 @@
 package com.example.myapplication1
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.os.*
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
+import com.example.myapplication1.databinding.ActivityMainBinding
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+
+const val REQUEST_CODE = 300
+
+class MainActivity : AppCompatActivity(), Timer.OnTimeTickListener {
+    private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private var permissionGranted = false
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var recorder: MediaRecorder
+    private var dirPath = ""
+    private var filename= ""
+    private var isRecording = false
+    private var isPaused = false
+
+    private lateinit var timer: Timer
+
+    private lateinit var vibrator: Vibrator
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        permissionGranted = ActivityCompat.checkSelfPermission(
+            this,
+            permissions[0]
+        ) == PackageManager.PERMISSION_GRANTED
+
+        timer = Timer(this)
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        if (!permissionGranted) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+        }
+
+        binding.btnRecord.setOnClickListener {
+            when{
+                isPaused -> resumeRecording()
+                isRecording -> pauseRecording()
+                else -> startRecording()
+            }
+
+            vibrator.vibrate(VibrationEffect.createOneShot(50,VibrationEffect.DEFAULT_AMPLITUDE))
+        }
+
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == REQUEST_CODE)
+            permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun pauseRecording(){
+        recorder.pause()
+        isPaused = true
+        binding.btnRecord.setImageResource(R.drawable.ic_mic)
+
+        timer.pause()
+    }
+
+    private fun resumeRecording(){
+        recorder.resume()
+        isPaused = false
+        binding.btnRecord.setImageResource(R.drawable.ic_pause)
+
+        timer.start()
+    }
+
+    private fun startRecording(){
+        if(!permissionGranted) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+            return
+        }
+        // start recording here
+
+        recorder = if(Build.VERSION.SDK_INT < 31) {
+            MediaRecorder()
+        } else {
+            MediaRecorder(this)
+        }
+        dirPath = "${externalCacheDir?.absolutePath}"
+        val simpleDateFormat = SimpleDateFormat("yyyy.MM.DD_hh.mm.ss")
+        val date = simpleDateFormat.format(Date())
+        filename = "audio_record_$date"
+
+        recorder.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile("$dirPath$filename.mp3")
+
+            try {
+                prepare()
+            }catch (e: IOException) {}
+
+            start()
+        }
+        binding.btnRecord.setImageResource(R.drawable.ic_pause)
+        isRecording = true
+        isPaused = false
+
+        timer.start()
+    }
+
+    private fun stopRecording(){
+        timer.stop()
+    }
+
+    override fun onTimerTick(duration: String) {
+        binding.tvTimer.text = duration
     }
 }
---
+++ app/src/main/java/com/example/myapplication1/Timer.kt
@@ -0,0 +1,54 @@
+package com.example.myapplication1
+
+import android.os.Looper
+import android.os.Handler
+
+class Timer(listener: OnTimeTickListener) {
+
+    interface OnTimeTickListener{
+        fun onTimerTick(duration: String)
+    }
+
+    private var handler = Handler(Looper.getMainLooper())
+    private lateinit var runnable: Runnable
+
+    private var duration = 0L
+    private var delay = 100L
+
+    init {
+        runnable = Runnable {
+            duration += delay
+            handler.postDelayed(runnable,delay)
+            listener.onTimerTick(format())
+        /* pass format to onTimeTick function through Runnable object
+            so that mainActivity receives when overriding this function
+        */
+        }
+    }
+
+    fun start(){
+        handler.postDelayed(runnable,delay)
+    }
+
+    fun pause(){
+        handler.removeCallbacks(runnable)
+    }
+
+    fun stop(){
+        handler.removeCallbacks(runnable)
+        duration = 0L
+    }
+
+    private fun format(): String {
+        val millis = duration % 1000
+        val seconds = (duration / 1000) % 60
+        val minutes = (duration / (1000 * 60)) % 60
+        val hours = (duration / (1000 * 60 * 60))
+
+        return if (hours > 0)
+            "%02d:%02d:%02d.%02d".format(hours, minutes, seconds, millis / 10)
+        else
+            "%02d:%02d.%02d".format(minutes, seconds, millis / 10)
+    }
+
+}
